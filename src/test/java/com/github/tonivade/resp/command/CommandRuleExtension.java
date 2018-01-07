d15dd8c06e74159a8cf02ec215d36594813d23bc
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-01-07 16:57:57 +0100

    minor cleanup

--- src/test/java/com/github/tonivade/resp/command/CommandRuleExtension.java
+++ src/test/java/com/github/tonivade/resp/command/CommandRuleExtension.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 
-public class CommandRuleExtension implements ParameterResolver
-{
+public class CommandRuleExtension implements ParameterResolver {
+
   @Override
   public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
       throws ParameterResolutionException {
