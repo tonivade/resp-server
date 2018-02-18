cc57f375ffe7e762dcc49f11e2d0b04ae1bc879e
Antonio Muñoz <antoniogmc@gmail.com>
2018-02-18 17:37:10 +0100

    improved junit 5 extension

--- src/test/java/com/github/tonivade/resp/RespServerExtension.java
+++ src/test/java/com/github/tonivade/resp/RespServerExtension.java
@@ -4,11 +4,11 @@
  */
 package com.github.tonivade.resp;
 
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class RespServerExtension implements BeforeEachCallback, AfterEachCallback {
+public class RespServerExtension implements BeforeAllCallback, AfterAllCallback {
 
   private final RespServer server;
 
@@ -17,14 +17,12 @@ public class RespServerExtension implements BeforeEachCallback, AfterEachCallbac
   }
   
   @Override
-  public void beforeEach(ExtensionContext context) throws Exception
-  {
+  public void beforeAll(ExtensionContext context) throws Exception {
     server.start();
   }
   
   @Override
-  public void afterEach(ExtensionContext context) throws Exception
-  {
+  public void afterAll(ExtensionContext context) throws Exception {
     server.stop();
   }
 }
