db0c41ce9e6ec4be50f4ac2258e3c897e33cc596
Antonio Muñoz <antoniogmc@gmail.com>
2021-09-08 20:28:09 +0200

    fixes some warnings in tests

--- src/test/java/com/github/tonivade/resp/protocol/RedisSerializerTest.java
+++ src/test/java/com/github/tonivade/resp/protocol/RedisSerializerTest.java
@@ -7,12 +7,11 @@ package com.github.tonivade.resp.protocol;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static com.github.tonivade.resp.protocol.RedisSerializer.encodeToken;
 
 import org.junit.jupiter.api.Test;
 
 public class RedisSerializerTest {
-
-  private RedisSerializer encoder = new RedisSerializer();
 
   private RedisToken intToken = RedisToken.integer(1);
   private RedisToken abcString = RedisToken.string("abc");
@@ -23,32 +22,32 @@ public class RedisSerializerTest {
 
   @Test
   public void encodeString() {
-    assertThat(encoder.encodeToken(abcString), equalTo("$3\r\nabc\r\n".getBytes(UTF_8)));
+    assertThat(encodeToken(abcString), equalTo("$3\r\nabc\r\n".getBytes(UTF_8)));
   }
 
   @Test
   public void encodeStatus() {
-    assertThat(encoder.encodeToken(pongString), equalTo("+pong\r\n".getBytes(UTF_8)));
+    assertThat(encodeToken(pongString), equalTo("+pong\r\n".getBytes(UTF_8)));
   }
 
   @Test
   public void encodeInteger() {
-    assertThat(encoder.encodeToken(intToken), equalTo(":1\r\n".getBytes(UTF_8)));
+    assertThat(encodeToken(intToken), equalTo(":1\r\n".getBytes(UTF_8)));
   }
 
   @Test
   public void encodeError() {
-    assertThat(encoder.encodeToken(errorString), equalTo("-ERR\r\n".getBytes(UTF_8)));
+    assertThat(encodeToken(errorString), equalTo("-ERR\r\n".getBytes(UTF_8)));
   }
 
   @Test
   public void encodeArray() {
-    assertThat(encoder.encodeToken(arrayToken), equalTo("*2\r\n:1\r\n$3\r\nabc\r\n".getBytes(UTF_8)));
+    assertThat(encodeToken(arrayToken), equalTo("*2\r\n:1\r\n$3\r\nabc\r\n".getBytes(UTF_8)));
   }
 
   @Test
   public void encodeArrayOfArrays() {
-    assertThat(encoder.encodeToken(arrayOfArraysToken),
+    assertThat(encodeToken(arrayOfArraysToken),
         equalTo("*2\r\n*2\r\n:1\r\n$3\r\nabc\r\n*2\r\n:1\r\n$3\r\nabc\r\n".getBytes(UTF_8)));
   }
 }
